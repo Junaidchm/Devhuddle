0ee1306bdfc86c421b67de8ef3e0c4931c4b8cd6
Junaid chammayil <80151977+812956@users.noreply.github.com>
2025-07-09 13:15:57 +0530

    test

--- auth-service/src/routes/auth.routes.ts
+++ auth-service/src/routes/auth.routes.ts
@@ -6,21 +6,6 @@ import jwtMiddleware from "../middleware/jwt.middleware";
 import refreshTokenMiddleware from "../middleware/refreshToken";
 import { requireRole } from "../middleware/role.middleware";
 
-// Configure multer for file uploads
-// const storage = multer.memoryStorage();
-// const upload = multer({
-//   storage,
-//   limits: { fileSize: 1024 * 1024 }, // 1MB limit
-//   fileFilter: (req, file, cb) => {
-//     const allowedTypes = ["image/jpeg", "image/png", "image/gif"];
-//     if (allowedTypes.includes(file.mimetype)) {
-//       cb(null, true);
-//     } else {
-//       cb(new Error("Invalid file type. Only JPG, PNG, or GIF allowed."));
-//     }
-//   },
-// });
-
 const router = Router();
 const authController: AuthController = new AuthController();
 
